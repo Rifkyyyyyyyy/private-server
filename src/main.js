1bc902280a15cfdfa68d7b10be9a45eb11926205
Rifkyyyyyyyy <zayx1231@gmail.com>
2025-07-26 09:32:27 +0700

    hapus http

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { web } from './application/web.js';
+import web from './application/web.js';
 import { logger } from './application/logger.js';
 
 const PORT = process.env.APP_PORT || 3000;
 
-web.listen(PORT, () => {
+web.web.listen(PORT, () => {
   logger.info(`✅ Aplikasi berjalan di PORT: ${PORT}`);
 });
